47d708c4b7ca03b98d98f2c74fb86aec6754d1f8
Tim Kelley <timkelley683@gmail.com>
2023-01-27 18:44:25 -0800

    completed login and register frontend

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,15 +1,50 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import Alert from '../common/Alert';
 
-const Login = () => {
+const Login = ({ login }) => {
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    username: "",
+    password: ""
+  });
+  const [formErrors, setFormErrors] = useState([]);
+
+  /** Handle form submit:
+   *
+   * Calls login func prop and, if successful, redirect to /companies.
+   */
+  async function handleSubmit(e){
+    e.preventDefault();
+
+    let result = await login(formData);
+    console.log(result)
+    if (result.success){
+      navigate("/");
+    } else {
+      setFormErrors(result.errors);
+    }
+  }
+
+  // Update form data field
+  function handleChange(e){
+    const { name, value } = e.target;
+    setFormData(l => ({...l, [name]: value }));
+  }
+  console.log(formData)
+  
+
   return (
     <div className="auth">
         <h1>Login</h1>
         <form>
-            <input type="text" placeholder="username" />
-            <input type="password" placeholder="password" />
-            <button>Login</button>
-            <p>This is an error!</p>
+            <input type="text" placeholder="username" name="username" required onChange={handleChange} />
+            <input type="password" placeholder="password" name="password" required onChange={handleChange} />
+            <button onClick={handleSubmit}>Login</button>
+
+            {formErrors.length
+              ? <Alert type="danger" messages={formErrors} />
+              : null}
             <span>Don't have an account? <Link to="/register">Sign Up</Link></span>
         </form>
     </div>
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,8 +1,15 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import axios from 'axios'
+import BlogStoneApi from '../api/api'
+import signup from "../App.js"
+import Alert from '../common/Alert'
 
-const Register = () => {
+
+const Register = ({signup}) => {
+  const navigate = useNavigate();
+  const [formErrors, setFormErrors] = useState([]);
+
   const [inputs, setInputs] = useState({
     username:"",
     email:"",
@@ -14,15 +21,29 @@ const Register = () => {
   }
   console.log(inputs);
 
-  const handleSubmit = async (e) => {
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+  //   try{
+  //   const res = await axios.post("http://localhost:3001/auth/register", inputs)
+  //   console.log(res)
+  //   } catch (err){
+  //     console.log(err)
+  //   }
+  // }
+
+  async function handleSubmit(e) {
     e.preventDefault();
-    try{
-    const res = await axios.post("http://localhost:3001/auth/register", inputs)
-    console.log(res)
-    } catch (err){
-      console.log(err)
+    
+    let result = await signup(inputs);
+    console.log(result)
+    if(result.success){
+    navigate("/");
+    } else {
+      setFormErrors(result.errors);
     }
   }
+  
+
   return (
     <div className="auth">
         <h1>Register</h1>
@@ -31,7 +52,10 @@ const Register = () => {
             <input required type="email" placeholder="email" name="email" onChange={handleChange} />
             <input required type="password" placeholder="password" name="password" onChange={handleChange} />
             <button onClick={handleSubmit}>Register</button>
-            <p>This is an error!</p>
+            
+            {formErrors.length
+              ? <Alert type="danger" messages={formErrors} />
+              : null}
             <span>Already have an account? <Link to="/login">Log In</Link></span>
         </form>
     </div>
