75a9214db2b1063687982c83eee5af5bbc7d1804
Tim Kelley <timkelley683@gmail.com>
2023-02-02 18:19:25 -0800

    add get post by id

--- src/pages/Single.jsx
+++ src/pages/Single.jsx
@@ -1,18 +1,62 @@
-import React from 'react'
-import { Link } from "react-router-dom"
+import React, {useState, useEffect} from 'react'
+import { Link, useLocation } from "react-router-dom"
 import Edit from "../img/pencil.png"
 import Delete from "../img/delete.png"
 import Menu from './Menu'
+import BlogStoneApi from '../api/api'
 
 const Single = () => {
+    const [post, setPost] = useState({});
+
+    const location = useLocation();
+
+    const id = location.pathname.split("/")[2]
+
+    // let cate = cat.split("=")
+    // let cate1 = cate[1];
+    // console.log("CATE", cate1);
+
+    // useEffect(() => {
+    //     const fetchData = async () => {
+    //         try{
+    //             const res = await axios.get(`/posts${cat}`);
+    //             setPosts(posts);
+    //         }catch (err) {
+    //             console.log(err);
+    //         }
+    //     };
+    //     fetchData();
+    // }, [cat]);
+
+    useEffect(function getPostsOnMount() {
+        console.debug("All Posts List useEffect getPostsOnMount");
+        search();
+    }, [id]);
+
+    async function search(){
+
+        let post = await BlogStoneApi.getPost(id)
+        setPost(post)
+
+        // if(!cat){
+        //     let posts = await BlogStoneApi.getAllPosts();
+        //     // console.log("IM ALL POSTS")
+        //     setPost(post)
+        // }else{
+        //     let posts = await BlogStoneApi.getPostsByCat(cat);
+        //     console.log("POSTS", posts)
+        //     console.log("CAT", cate1);
+        //     setPost(post);
+        // }
+    }
   return (
     <div className="single">
         <div className="content">
-            <img src="https://images.unsplash.com/photo-1499951360447-b19be8fe80f5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"></img>
+            <img src={post.img}></img>
             <div className="user">
                 <img src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8dXNlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="user image" />
                 <div className="info">
-                    <span>John</span>
+                    <span>{post.username}</span>
                     <p>Posted 2 Days ago</p>
                 </div>
                 <div className="edit">
@@ -22,15 +66,9 @@ const Single = () => {
                     <img src={Delete} alt="" />
                 </div>   
             </div>
-            <h1>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto quo ipsam minus veritatis placeat magni natus, delectus, dolores eius explicabo aspernatur </h1>
+            <h1>{post.title}</h1>
             <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, eius perferendis animi qui nostrum optio ipsam dignissimos, eum ipsa error facilis ea deleniti? Provident magni, officia eveniet accusamus animi blanditiis.
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Non inventore iusto necessitatibus aliquam eveniet pariatur eius similique deserunt? Accusamus earum officiis facilis nulla eaque neque architecto nemo magnam qui recusandae.
-                <br />
-                <br />
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora cum excepturi, obcaecati voluptatibus beatae laboriosam vitae, necessitatibus iusto expedita a commodi doloribus dolore quisquam non accusamus provident? Neque, nihil cupiditate!
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam debitis in, voluptatem itaque corporis nesciunt possimus, eius tenetur ipsa molestias ut vitae tempora! Commodi atque quibusdam quae illum, debitis ipsam.
-
+                {post.content}
             </p>
         
         </div>
