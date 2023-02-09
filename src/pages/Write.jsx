38c13441e0f2ce75df8a1b460d8b3058fc0fefe7
Tim Kelley <timkelley683@gmail.com>
2023-02-08 16:24:36 -0800

    omitted React Quill

--- src/pages/Write.jsx
+++ src/pages/Write.jsx
@@ -31,29 +31,38 @@ const Write = () => {
 
     const [formData, setFormData] = useState({
         title: "",
-        content: "",
+        content: value,
         img: "",
+        cat: ""
         
     })
+    console.log(formData)
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        let res = await BlogStoneApi.addPost(formData)
+        let res = await BlogStoneApi.addPost(formData, value)
         console.log("RESREACT", res)
         if(res.success){
             navigate("/")
         }
     }
 
+    const handleChange = async (e) => {
+        const { name, value } = e.target;
+        console.log(e.target.value)
+        setFormData(data => ({...data, [name]: value}));
+    }
+
     
   return (
     <div className="add">
         <div className="content">
-            <input type="text" placeholder="Title" onChange={e=>setTitle(e.target.value)} />
-            <input type="text" placeholder="Image URL" onChange={e=>setImg(e.target.value)} />
-            <div className="editorContainer">
-                <ReactQuill className="editor" theme="snow" value={value} onChange={setValue} />
-            </div>
+            <input type="text" placeholder="Title" name="title" onChange={handleChange} />
+            <input type="text" placeholder="Image URL" name="img" onChange={handleChange} />
+            <input className="editorContainer" type="text-area" name="content" onChange={handleChange} placeholder="Post content goes here..." />
+            {/* <div className="editorContainer">
+                <ReactQuill className="editor" theme="snow" name="content" value={value} onChange={setValue} />
+            </div> */}
         </div>
         <div className="menu">
             <div className="item">
@@ -74,27 +83,27 @@ const Write = () => {
             <div className="item">
                 <h1>Category</h1>
                 <div className="cat">
-                    <input type="radio" name="cat" value="art" id="art" onChange={e=>setCat(e.target.value)} />
+                    <input type="radio" name="cat" value="art" id="art" onChange={handleChange} />
                     <label htmlFor="art">Art</label>
                 </div>
                 <div className="cat">
-                    <input type="radio" name="cat" value="science" id="science" onChange={e=>setCat(e.target.value)} />
+                    <input type="radio" name="cat" value="science" id="science" onChange={handleChange} />
                     <label htmlFor="science">Science</label>
                 </div>
                 <div className="cat">
-                    <input type="radio" name="cat" value="technology" id="technology" onChange={e=>setCat(e.target.value)} />
+                    <input type="radio" name="cat" value="technology" id="technology" onChange={handleChange} />
                     <label htmlFor="technology">Technology</label>
                 </div>
                 <div className="cat">
-                    <input type="radio" name="cat" value="cinema" id="cinema" onChange={e=>setCat(e.target.value)} />
+                    <input type="radio" name="cat" value="cinema" id="cinema" onChange={handleChange} />
                     <label htmlFor="cinema">Cinema</label>
                 </div>
                 <div className="cat">
-                    <input type="radio" name="cat" value="design" id="design" onChange={e=>setCat(e.target.value)} />
+                    <input type="radio" name="cat" value="design" id="design" onChange={handleChange} />
                     <label htmlFor="design">Design</label>
                 </div> 
                 <div className="cat">
-                    <input type="radio" name="cat" value="food" id="food" onChange={e=>setCat(e.target.value)} />
+                    <input type="radio" name="cat" value="food" id="food" onChange={handleChange} />
                     <label htmlFor="food">Food</label>
                 </div>
             </div>
